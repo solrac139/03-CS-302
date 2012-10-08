d7d960eefd4306f0b9a52be10cb1e92afca028bd
Asher Dunn <dunn@unr.com>
2012-10-07 18:01:45 -0700

    Header and source file for bookings :=event.h.

---
+++ Scheduler/bookings.cpp
@@ -0,0 +1,12 @@
+#include "bookings.h"
+
+using namespace std;
+
+bookings::bookings(void)
+{
+}
+
+
+bookings::~bookings(void)
+{
+}
---
+++ Scheduler/bookings.h
@@ -0,0 +1,14 @@
+#pragma once
+class bookings
+{
+public:
+	bookings(void);
+	// Observers
+	
+	//
+	~bookings(void);
+private:
+	string magician;
+	string customer;
+};
+
